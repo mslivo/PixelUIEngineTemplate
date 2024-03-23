73ef23ed1fc054d2050eacb1325a582b4b7c4c24
Markus <markus.slivovksy89@gmail.com>
2024-03-23 11:18:22 +0100

    test

--- core/src/main/java/net/mslivo/uitemplate/core/cfg/CFG_MAIN.java
+++ core/src/main/java/net/mslivo/uitemplate/core/cfg/CFG_MAIN.java
@@ -3,7 +3,7 @@ package net.mslivo.uitemplate.core.cfg;
 import net.mslivo.core.engine.ui_engine.enums.VIEWPORT_MODE;
 
 public class CFG_MAIN {
-    public static final String APP_TITLE = "UITemplate";
+    public static final String APP_TITLE = "uitemplate";
     public static final int TARGET_FPS = 60;
     public static final int RESOLUTION_WIDTH = 256;
     public static final int RESOLUTION_HEIGHT = 240;
